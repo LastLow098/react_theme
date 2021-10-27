287a18b8cf03baad51c189b2905d4aff9445e3f5
Алексей Карпов <73760173+LastLow098@users.noreply.github.com>
2021-10-27 19:51:10 +0300

    v0.2

--- src/Range.js
+++ src/Range.js
@@ -1,17 +1,61 @@
-function Range () {
-    return (
-        <div className="range">
-            <div className="range_text">
-                <div className="range_text_items">Сумма займа</div>
-                <div class="range_text_items">138100 P</div>
+import React, {Component} from 'react'
+
+class Range extends Component {
+    state = {
+        range1: "13.8",
+        range2: "66.6"
+    }
+
+    toCurrency = price => {
+        return new Intl.NumberFormat('ru-RU', {
+            currency: 'rub',
+            style: 'currency',
+            minimumFractionDigits: 0
+        }).format(price)
+    }
+
+    changeRange = (id) => {
+        let rangeChan = document.querySelector('#'+ id)
+        var rangeValueNew
+        if (id === 'range1') {
+            rangeValueNew = ((rangeChan.value - 15000) * 100) / 1000000
+        }else {
+            rangeValueNew = ((rangeChan.value - 2) * 100) / 10
+        }
+        rangeChan.style.background = "-webkit-linear-gradient(left, #67ac5b 0%, #67ac5b "+ rangeValueNew +"%, rgb(215,216,220) "+ rangeValueNew +"%, rgb(215,216,220) 100%)"
+    }
+
+    render() {
+        const styleRange = {
+            background: '-webkit-linear-gradient(left, #67ac5b 0%, #67ac5b 60%, #d7d8dc 60%, #d7d8dc 100%)'
+        }
+        return (
+            <div>
+                <div className="range">
+                    <div className="range_text">
+                        <div className="range_text_items">Сумма займа</div>
+                        <div className="range_text_items">{this.toCurrency(138100)}</div>
+                    </div>
+                    <input type="range" className="range_sub" id="range1" min="15000" max="1000000" defaultValue="138000" onChange={this.changeRange.bind(this, 'range1')} />
+                    <div className="range_sub_items">
+                        <div className="range_sub_item">от {this.toCurrency(15000)}</div>
+                        <div className="range_sub_item">до {this.toCurrency(1000000)}</div>
+                    </div>
+                </div>
+                <div className="range">
+                    <div className="range_text">
+                        <div className="range_text_items">Срок займа</div>
+                        <div className="range_text_items">8 месяцев</div>
+                    </div>
+                    <input type="range" style={styleRange} className="range_sub" id="range2" min="2" max="12" defaultValue="8" onChange={this.changeRange.bind(this, 'range2')} />
+                    <div className="range_sub_items">
+                        <div className="range_sub_item">от 2 месяцев</div>
+                        <div className="range_sub_item">до 12 месяцев</div>
+                    </div>
+                </div>
             </div>
-            <input type="range" className="range_sub" min="15000" max="1000000" value="138000" />
-            <div className="range_sub_items">
-            <div class="range_sub_item">от 15000</div>
-            <div class="range_sub_item">до 1000000</div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Range
